1ff492cb0efd155667d012e04a2adf5081769258
jnape <jnape09@gmail.com>
2020-05-04 19:59:59 -0500

    interop.Shoki supports Iterable inputs

--- src/main/java/com/jnape/palatable/shoki/interop/Shoki.java
+++ src/main/java/com/jnape/palatable/shoki/interop/Shoki.java
@@ -8,10 +8,12 @@ import com.jnape.palatable.shoki.impl.HashSet;
 import com.jnape.palatable.shoki.impl.StrictQueue;
 import com.jnape.palatable.shoki.impl.StrictStack;
 
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Objects;
+import java.util.RandomAccess;
 
 import static com.jnape.palatable.lambda.functions.Fn2.curried;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
@@ -31,31 +33,29 @@ public final class Shoki {
     }
 
     /**
-     * Construct a {@link StrictStack} from an input {@link java.util.Collection}, preserving the same ordering of
-     * elements.
+     * Construct a {@link StrictStack} from an input {@link Iterable}, preserving the same ordering of elements.
      * <p>
      * Due to the {@link StrictStack#cons(Object) lifo} nature of {@link StrictStack}, this method makes an effort to
-     * iterate the elements of the input {@link java.util.Collection collection} in reverse, if a known type
-     * representing that capability has been advertised by the {@link java.util.Collection collection}. If no reverse
-     * iteration strategy can be deduced from the input {@link java.util.Collection collection}, the elements will be
-     * {@link StrictStack#cons(Object) cons'ed} onto the {@link StrictStack} in one pass and the {@link StrictStack}
-     * will be {@link StrictStack#reverse() reversed} before being returned.
+     * iterate the elements of the input {@link Iterable} in reverse, if a known type representing that capability has
+     * been advertised by the {@link Iterable}. If no reverse iteration strategy can be deduced from the input
+     * {@link Iterable}, the elements will be {@link StrictStack#cons(Object) cons'ed} onto the {@link StrictStack} in
+     * one pass and the {@link StrictStack} will be {@link StrictStack#reverse() reversed} before being returned.
      *
-     * @param javaCollection the input {@link java.util.Collection}
-     * @param <A>            the element type
+     * @param javaIterable the input {@link Iterable}
+     * @param <A>          the element type
      * @return the populated {@link StrictStack}
      */
-    public static <A> StrictStack<A> strictStack(java.util.Collection<A> javaCollection) {
-        return javaCollection instanceof java.util.Deque<?>
-               ? foldLeft(StrictStack::cons, StrictStack.empty(),
-                          ((java.util.Deque<A>) javaCollection)::descendingIterator)
-               : javaCollection instanceof java.util.List<?>
-                 ? foldLeft(StrictStack::cons, StrictStack.empty(),
-                            javaCollection instanceof java.util.RandomAccess
-                            ? map(((java.util.List<A>) javaCollection)::get,
-                                  takeWhile(gte(0), iterate(i -> i - 1, javaCollection.size() - 1)))
+    public static <A> StrictStack<A> strictStack(Iterable<A> javaIterable) {
+        if (javaIterable instanceof Deque<?>)
+            return foldLeft(StrictStack::cons, StrictStack.empty(), ((Deque<A>) javaIterable)::descendingIterator);
+
+        if (javaIterable instanceof List<?>) {
+            List<A> javaList = (List<A>) javaIterable;
+            return foldLeft(StrictStack::cons, StrictStack.empty(),
+                            javaList instanceof RandomAccess
+                            ? map(javaList::get, takeWhile(gte(0), iterate(i -> i - 1, javaList.size() - 1)))
                             : () -> {
-                                ListIterator<A> itr = ((List<A>) javaCollection).listIterator(javaCollection.size());
+                                ListIterator<A> itr = javaList.listIterator(javaList.size());
                                 return new Iterator<A>() {
                                     @Override
                                     public boolean hasNext() {
@@ -67,20 +67,21 @@ public final class Shoki {
                                         return itr.previous();
                                     }
                                 };
-                            })
-                 : foldLeft(StrictStack::cons, StrictStack.<A>empty(), javaCollection).reverse();
+                            });
+        }
+
+        return foldLeft(StrictStack::cons, StrictStack.<A>empty(), javaIterable).reverse();
     }
 
     /**
-     * Construct a {@link StrictQueue} from an input {@link java.util.Collection}, preserving the same ordering of
-     * elements.
+     * Construct a {@link StrictQueue} from an input {@link Iterable}, preserving the same ordering of elements.
      *
-     * @param javaCollection the input {@link java.util.Collection}
-     * @param <A>            the element type
+     * @param javaIterable the input {@link Iterable}
+     * @param <A>          the element type
      * @return the populated {@link StrictQueue}
      */
-    public static <A> StrictQueue<A> strictQueue(java.util.Collection<A> javaCollection) {
-        return foldLeft(StrictQueue::snoc, StrictQueue.empty(), javaCollection);
+    public static <A> StrictQueue<A> strictQueue(Iterable<A> javaIterable) {
+        return foldLeft(StrictQueue::snoc, StrictQueue.empty(), javaIterable);
     }
 
     /**
@@ -98,16 +99,16 @@ public final class Shoki {
     }
 
     /**
-     * Construct a {@link HashSet} from an input {@link java.util.Set}, using
+     * Construct a {@link HashSet} from an input {@link Iterable}, using
      * {@link Objects#equals(Object, Object) Object equality} and {@link Objects#hashCode(Object) Object hashCode} as
      * the {@link EquivalenceRelation} and {@link HashingAlgorithm}, respectively, for its elements.
      *
-     * @param javaCollection the input {@link java.util.Collection}
-     * @param <A>            the element type
+     * @param javaIterable the input {@link Iterable}
+     * @param <A>          the element type
      * @return the populated {@link HashSet}
      */
-    public static <A> HashSet<A> hashSet(java.util.Collection<A> javaCollection) {
-        return foldLeft(HashSet::add, HashSet.empty(), javaCollection);
+    public static <A> HashSet<A> hashSet(Iterable<A> javaIterable) {
+        return foldLeft(HashSet::add, HashSet.empty(), javaIterable);
     }
 
     /**
@@ -130,15 +131,15 @@ public final class Shoki {
     }
 
     /**
-     * Construct a {@link HashMultiSet} from an input {@link java.util.Collection}, using
+     * Construct a {@link HashMultiSet} from an input {@link Iterable}, using
      * {@link Objects#equals(Object, Object) Object equality} and {@link Objects#hashCode(Object) Object hashCode} as
      * the {@link EquivalenceRelation} and {@link HashingAlgorithm}, respectively, for its elements.
      *
-     * @param javaCollection the input {@link java.util.Collection}
-     * @param <A>            the element type
+     * @param javaIterable the input {@link Iterable}
+     * @param <A>          the element type
      * @return the populated {@link HashMultiSet}
      */
-    public static <A> HashMultiSet<A> hashMultiSet(java.util.Collection<A> javaCollection) {
-        return foldLeft(HashMultiSet::inc, HashMultiSet.empty(), javaCollection);
+    public static <A> HashMultiSet<A> hashMultiSet(Iterable<A> javaIterable) {
+        return foldLeft(HashMultiSet::inc, HashMultiSet.empty(), javaIterable);
     }
 }

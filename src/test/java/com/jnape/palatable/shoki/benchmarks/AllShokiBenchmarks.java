0a0b19de96a1a06c12f2d5e1a73f045a771ecd0d
jnape <jnape09@gmail.com>
2020-05-11 13:25:28 -0500

    Adding shortcut for running benchmarks for all shoki types
    - updating benchmarks

---
+++ src/test/java/com/jnape/palatable/shoki/benchmarks/AllShokiBenchmarks.java
@@ -0,0 +1,17 @@
+package com.jnape.palatable.shoki.benchmarks;
+
+import org.openjdk.jmh.runner.RunnerException;
+
+public final class AllShokiBenchmarks {
+    private AllShokiBenchmarks() {
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        NaturalBenchmark.main(args);
+        StrictStackBenchmark.Shoki.main(args);
+        StrictQueueBenchmark.Shoki.main(args);
+        HashMapBenchmark.Shoki.main(args);
+        HashSetBenchmark.Shoki.main(args);
+        HashMultiSetBenchmark.Shoki.main(args);
+    }
+}
